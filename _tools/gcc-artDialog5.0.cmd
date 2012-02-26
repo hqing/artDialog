1ecdcc40496bd7962d0b7d10918cc09965567a31
1987.tangbin@gmail.com <1987.tangbin@gmail.com@03965403-1007-c2db-2469-1405ecafa057>
2012-02-26 15:31:34 +0000

    git-svn-id: http://artdialog.googlecode.com/svn/trunk@32 03965403-1007-c2db-2469-1405ecafa057

---
+++ _tools/gcc-artDialog5.0.cmd
@@ -0,0 +1,5 @@
+:: ÉèÖÃÄ¿Â¼Ãû
+set jsPath=artDialog5.0
+
+java -jar compiler.jar --js "../%jsPath%/source/artDialog.js" --js_output_file "../%jsPath%/artDialog.min.js"
+java -jar compiler.jar --js "../%jsPath%/source/jquery.artDialog.js" --js_output_file "../%jsPath%/jquery.artDialog.min.js"
