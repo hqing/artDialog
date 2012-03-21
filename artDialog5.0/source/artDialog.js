9b0c23e0729a7e008e283dde4a00d9e90488f523
1987.tangbin@gmail.com <1987.tangbin@gmail.com@03965403-1007-c2db-2469-1405ecafa057>
2012-03-21 14:42:02 +0000

    git-svn-id: http://artdialog.googlecode.com/svn/trunk@41 03965403-1007-c2db-2469-1405ecafa057

--- artDialog5.0/source/artDialog.js
+++ artDialog5.0/source/artDialog.js
@@ -1,6 +1,6 @@
 /*!
 * artDialog 5
-* Date: 2012-02-28
+* Date: 2012-03-21
 * http://code.google.com/p/artdialog/
 * (c) 2009-2012 TangBin, http://www.planeArt.cn
 *
@@ -672,11 +672,11 @@ artDialog.fn = artDialog.prototype = {
     
     /** @inner */
     constructor: function (config) {
-        var that = this, dom;
+        var dom;
         
-        that.closed = false;
-        that.config = config;
-        that.dom = dom = that.dom || that._getDom();
+        this.closed = false;
+        this.config = config;
+        this.dom = dom = this.dom || this._getDom();
         
         config.skin && dom.wrap.addClass(config.skin);
         
@@ -684,28 +684,28 @@ artDialog.fn = artDialog.prototype = {
         dom.close[config.cancel === false ? 'hide' : 'show']();
         dom.content.css('padding', config.padding);
         
-        that.button.apply(that, config.button);
+        this.button.apply(this, config.button);
         
-        that.title(config.title)
+        this.title(config.title)
         .content(config.content)
         .size(config.width, config.height)
         .time(config.time);
         
         config.follow
-        ? that.follow(config.follow)
-        : that.position();
+        ? this.follow(config.follow)
+        : this.position();
         
-        that.zIndex();
-        config.lock && that.lock();
+        this.zIndex();
+        config.lock && this.lock();
         
-        that._addEvent();
-        that[config.visible ? 'visible' : 'hidden']().focus();
+        this._addEvent();
+        this[config.visible ? 'visible' : 'hidden']().focus();
         
         _singleton = null;
         
-        config.initialize && config.initialize.call(that);
+        config.initialize && config.initialize.call(this);
         
-        return that;
+        return this;
     },
     
     
@@ -717,13 +717,13 @@ artDialog.fn = artDialog.prototype = {
     
         var prev, next, parent, display,
             that = this,
-            $content = that.dom.content,
+            $content = this.dom.content,
             content = $content[0];
         
         
-        if (that._elemBack) {
-            that._elemBack();
-            delete that._elemBack;
+        if (this._elemBack) {
+            this._elemBack();
+            delete this._elemBack;
         };
         
         
@@ -740,7 +740,7 @@ artDialog.fn = artDialog.prototype = {
             next = message.nextSibling;
             parent = message.parentNode;
             
-            that._elemBack = function () {
+            this._elemBack = function () {
                 if (prev && prev.parentNode) {
                     prev.parentNode.insertBefore(message, prev.nextSibling);
                 } else if (next && next.parentNode) {
@@ -758,7 +758,7 @@ artDialog.fn = artDialog.prototype = {
             
         };
         
-        return that.position();
+        return this.position();
     },
     
     
@@ -788,12 +788,11 @@ artDialog.fn = artDialog.prototype = {
     /** @inner 位置居中 */
     position: function () {
     
-        var that = this,
-            dom = that.dom,
+        var dom = this.dom,
             wrap = dom.wrap[0],
             $window = dom.window,
             $document = dom.document,
-            fixed = that.config.fixed,
+            fixed = this.config.fixed,
             dl = fixed ? 0 : $document.scrollLeft(),
             dt = fixed ? 0 : $document.scrollTop(),
             ww = $window.width(),
@@ -807,7 +806,7 @@ artDialog.fn = artDialog.prototype = {
         style.left = Math.max(left, dl) + 'px';
         style.top = Math.max(top, dt) + 'px';
         
-        return that;
+        return this;
     },
     
     
@@ -842,19 +841,18 @@ artDialog.fn = artDialog.prototype = {
     follow: function (elem) {
     
         var $elem = $(elem),
-            that = this,
-            config = that.config;
+            config = this.config;
         
         
         // 隐藏元素不可用
         if (!elem || !elem.offsetWidth && !elem.offsetHeight) {
         
-            return that.position(that._left, that._top);
+            return this.position(this._left, this._top);
         };
         
         var fixed = config.fixed,
             expando = _expando + 'follow',
-            dom = that.dom,
+            dom = this.dom,
             $window = dom.window,
             $document = dom.document,
             winWidth = $window.width(),
@@ -866,7 +864,7 @@ artDialog.fn = artDialog.prototype = {
             height = elem.offsetHeight,
             left = fixed ? offset.left - docLeft : offset.left,
             top = fixed ? offset.top - docTop : offset.top,
-            wrap = that.dom.wrap[0],
+            wrap = this.dom.wrap[0],
             style = wrap.style,
             wrapWidth = wrap.offsetWidth,
             wrapHeight = wrap.offsetHeight,
@@ -892,11 +890,11 @@ artDialog.fn = artDialog.prototype = {
         style.top = setTop + 'px';
         
         
-        that._follow && that._follow.removeAttribute(expando);
-        that._follow = elem;
+        this._follow && this._follow.removeAttribute(expando);
+        this._follow = elem;
         elem[expando] = config.id;
         
-        return that;
+        return this;
     },
     
     
@@ -912,12 +910,11 @@ artDialog.fn = artDialog.prototype = {
     */
     button: function () {
     
-        var that = this,
-            dom = that.dom,
+        var dom = this.dom,
             $buttons = dom.buttons,
             elem = $buttons[0],
             strongButton = 'd-state-highlight',
-            listeners = that._listeners = that._listeners || {},
+            listeners = this._listeners = this._listeners || {},
             ags = [].slice.call(arguments);
             
         var i = 0, val, value, id, isNewButton, button;
@@ -951,9 +948,9 @@ artDialog.fn = artDialog.prototype = {
             };
             
             if (val.focus) {
-                that._focus && that._focus.removeClass(strongButton);
-                that._focus = $(button).addClass(strongButton);
-                that.focus();
+                this._focus && this._focus.removeClass(strongButton);
+                this._focus = $(button).addClass(strongButton);
+                this.focus();
             };
             
             button[_expando + 'callback'] = id;
@@ -968,7 +965,7 @@ artDialog.fn = artDialog.prototype = {
         
         $buttons[0].style.display = ags.length ? '' : 'none';
         
-        return that;
+        return this;
     },
     
     
@@ -1003,23 +1000,22 @@ artDialog.fn = artDialog.prototype = {
     /** 关闭对话框 */
     close: function () {
     
-        var that = this,
-            dom = that.dom,
+        if (this.closed) {
+            return this;
+        };
+    
+        var dom = this.dom,
             $wrap = dom.wrap,
             list = artDialog.list,
-            beforeunload = that.config.beforeunload,
-            follow = that.config.follow;
+            beforeunload = this.config.beforeunload,
+            follow = this.config.follow;
         
-        if (that.closed) {
-            return that;
-        };
-        
-        if (beforeunload && beforeunload.call(that) === false) {
-            return that;
+        if (beforeunload && beforeunload.call(this) === false) {
+            return this;
         };
         
         
-        if (artDialog.focus === that) {
+        if (artDialog.focus === this) {
             artDialog.focus = null;
         };
         
@@ -1029,16 +1025,16 @@ artDialog.fn = artDialog.prototype = {
         };
         
         
-        if (that._elemBack) {
-            that._elemBack();
+        if (this._elemBack) {
+            this._elemBack();
         };
         
         
         
-        that.time();
-        that.unlock();
-        that._removeEvent();
-        delete list[that.config.id];
+        this.time();
+        this.unlock();
+        this._removeEvent();
+        delete list[this.config.id];
 
         
         if (_singleton) {
@@ -1048,7 +1044,7 @@ artDialog.fn = artDialog.prototype = {
         // 使用单例模式
         } else {
         
-            _singleton = that;
+            _singleton = this;
             
             dom.title.html('');
             dom.content.html('');
@@ -1063,18 +1059,18 @@ artDialog.fn = artDialog.prototype = {
                 position: _isFixed ? 'fixed' : 'absolute'
             });
             
-            for (var i in that) {
-                if (that.hasOwnProperty(i) && i !== 'dom') {
-                    delete that[i];
+            for (var i in this) {
+                if (this.hasOwnProperty(i) && i !== 'dom') {
+                    delete this[i];
                 };
             };
             
-            that.hidden();
+            this.hidden();
             
         };
         
-        that.closed = true;
-        return that;
+        this.closed = true;
+        return this;
     },
     
     
@@ -1085,56 +1081,54 @@ artDialog.fn = artDialog.prototype = {
     time: function (time) {
     
         var that = this,
-            timer = that._timer;
+            timer = this._timer;
             
         timer && clearTimeout(timer);
         
         if (time) {
-            that._timer = setTimeout(function(){
+            this._timer = setTimeout(function(){
                 that._click('cancel');
             }, time);
         };
         
         
-        return that;
+        return this;
     },
     
     /** @inner 设置焦点 */
     focus: function () {
-        var that = this;
-        
-        if (that.config.focus) {
+
+        if (this.config.focus) {
             //setTimeout(function () {
                 try {
-                    var elem = that._focus && that._focus[0] || that.dom.close[0];
+                    var elem = this._focus && this._focus[0] || this.dom.close[0];
                     elem && elem.focus();
                 // IE对不可见元素设置焦点会报错
                 } catch (e) {};
             //}, 0);
         };
         
-        return that;
+        return this;
     },
     
     
     /** 置顶对话框 */
     zIndex: function () {
     
-        var that = this,
-            dom = that.dom,
+        var dom = this.dom,
             top = artDialog.focus,
             index = artDialog.defaults.zIndex ++;
         
         // 设置叠加高度
         dom.wrap.css('zIndex', index);
-        that._lockMask && that._lockMask.css('zIndex', index - 1);
+        this._lockMask && this._lockMask.css('zIndex', index - 1);
         
         // 设置最高层的样式
         top && top.dom.outer.removeClass('d-state-focus');
-        artDialog.focus = that;
+        artDialog.focus = this;
         dom.outer.addClass('d-state-focus');
         
-        return that;
+        return this;
     },
     
     
@@ -1146,13 +1140,13 @@ artDialog.fn = artDialog.prototype = {
         };
         
         var that = this,
-            config = that.config,
-            dom = that.dom,
+            config = this.config,
+            dom = this.dom,
             div = document.createElement('div'),
             $div = $(div),
             index = artDialog.defaults.zIndex - 1;
         
-        that.zIndex();
+        this.zIndex();
         dom.outer.addClass('d-state-lock');
             
         $div.css({
@@ -1182,29 +1176,28 @@ artDialog.fn = artDialog.prototype = {
         
         document.body.appendChild(div);
         
-        that._lockMask = $div;
-        that._isLock = true;
+        this._lockMask = $div;
+        this._isLock = true;
         
-        return that;
+        return this;
     },
     
     
     /** 解开屏锁 */
     unlock: function () {
-        var that = this;
-        
-        if (!that._isLock) {
-            return that;
+
+        if (!this._isLock) {
+            return this;
         };
         
-        that._lockMask.unbind();
-        that._lockMask.hide();
-        that._lockMask.remove();
+        this._lockMask.unbind();
+        this._lockMask.hide();
+        this._lockMask.remove();
         
-        that.dom.outer.removeClass('d-state-lock');
-        that._isLock = false;
+        this.dom.outer.removeClass('d-state-lock');
+        this._isLock = false;
 
-        return that;
+        return this;
     },
     
     
@@ -1247,11 +1240,10 @@ artDialog.fn = artDialog.prototype = {
     // 按钮回调函数触发
     _click: function (id) {
     
-        var that = this,
-            fn = that._listeners[id] && that._listeners[id].callback;
+        var fn = this._listeners[id] && this._listeners[id].callback;
             
-        return typeof fn !== 'function' || fn.call(that) !== false ?
-            that.close() : that;
+        return typeof fn !== 'function' || fn.call(this) !== false ?
+            this.close() : this;
     },
     
     
@@ -1266,7 +1258,7 @@ artDialog.fn = artDialog.prototype = {
     _addEvent: function () {
     
         var that = this,
-            dom = that.dom;
+            dom = this.dom;
         
         
         // 监听点击
@@ -1529,6 +1521,7 @@ this.artDialog = $.dialog = $.artDialog = artDialog;
 27. 判断 DOCTYPE, 对 xhtml1.0 以下的页面报告错误
 28. 修复 IE8 动态等新内容时没有撑开对话框高度，特意为 ie8 取消 .d-content { display:inline-block }
 29. show 参数与方法更名为 visible
+30. 修正重复调用 close 方法出现的错误
 
 */
 
